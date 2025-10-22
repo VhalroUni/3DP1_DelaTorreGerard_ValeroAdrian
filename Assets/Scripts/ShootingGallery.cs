d01e9b2b22088c2f03d6f53e838bbdb7b3aa0c4d
VhalroUni <avalerof@edu.tecnocampus.cat>
2025-10-22 20:54:22 +0200

    cosas

--- Assets/Scripts/ShootingGallery.cs
+++ Assets/Scripts/ShootingGallery.cs
@@ -2,11 +2,26 @@ using UnityEngine;
 
 public class ShootingGallery : MonoBehaviour
 {
-    public int m_ScoreValue = 10;
-    public ShootingGalleryZone m_Zone;
+    public int scoreValue = 10;
+    private bool isHit = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            // Reactivar la diana si está desactivada (hit)
+            if (isHit)
+            {
+                gameObject.SetActive(true);
+                isHit = false;
+            }
+        }
+    }
+
     public void HitTarget()
     {
-        m_Zone.AddScore(m_ScoreValue);
-        GameObject.Destroy(gameObject);
+        // Aquí puedes añadir lógica de puntuación si quieres
+        isHit = true;
+        gameObject.SetActive(false);
     }
 }
