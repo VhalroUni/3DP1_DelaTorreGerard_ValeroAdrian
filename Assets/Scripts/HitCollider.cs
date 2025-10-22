e916d275528ab5594afa8370c7a5144366903191
gere149 <gere.delatorre@gmail.com>
2025-10-22 23:52:29 +0200

    Cambio de escena arreglado

--- Assets/Scripts/HitCollider.cs
+++ Assets/Scripts/HitCollider.cs
@@ -4,10 +4,20 @@ public class HitCollider : MonoBehaviour
 {
     public int m_Damage;
     public EnemyController m_Enemy;
-
+    void Awake()
+    {
+        if (m_Enemy == null)
+            m_Enemy = GetComponentInParent<EnemyController>();
+    }
     public void Hit()
     {
-        Debug.Log("Disparo acertado");
-        m_Enemy.Hit(m_Damage);
+        if (m_Enemy != null)
+        {
+            Debug.Log("Disparo acertado");
+            m_Enemy.Hit(m_Damage);
+        }
+        else
+            Debug.Log("No acertado");
+     
     }
 }
